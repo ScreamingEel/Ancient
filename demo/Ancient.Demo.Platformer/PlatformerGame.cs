ce627921b3cbd1f0e81d186f72a0c24874bff15d
Jonas O <31914237+Joonke96@users.noreply.github.com>
2022-03-29 00:16:13 +0200

    Added demo project

---
+++ demo/Ancient.Demo.Platformer/PlatformerGame.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Ancient.Demo.Platformer
+{
+    public class PlatformerGame : Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch? _spriteBatch;
+        private Color _drawColor = Color.CornflowerBlue;
+
+        public PlatformerGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new(GraphicsDevice);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (gameTime.TotalGameTime.Seconds > 30)
+                _drawColor = Color.Black;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(_drawColor);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ demo/Ancient.Demo.Platformer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Ancient.Demo.Platformer
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PlatformerGame())
+                game.Run();
+        }
+    }
+}
